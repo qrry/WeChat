21b2d6e00be79acefee3f7300ce8821fd359fc72
noneykd <459230404@qq.com>
2015-10-27 09:36:13 +0800

    调整项目结构

---
+++ Weixin/src/main/java/com/noneykd/test/WeixinTest.java
@@ -0,0 +1,32 @@
+package com.noneykd.test;
+
+import net.sf.json.JSONObject;
+
+import com.noneykd.menu.Menu;
+import com.noneykd.po.AccessToken;
+import com.noneykd.util.WeixinUtil;
+
+public class WeixinTest {
+	public static void main(String[] args) {
+		try {
+			AccessToken token = WeixinUtil.getAccessToken();
+			System.out.println("票据"+token.getToken());
+			System.out.println("有效时间"+token.getExpiresIn());
+			
+			//String path = "D:/imooc.jpg";
+			//String mediaId = WeixinUtil.upload(path, token.getToken(), "thumb");
+			//System.out.println(mediaId);
+			
+			Menu menu = WeixinUtil.initMenu();
+			String menuStr = JSONObject.fromObject(menu).toString();
+			int result = WeixinUtil.createMenu(token.getToken(), menuStr);
+			System.out.println(result);
+			
+//			String result = WeixinUtil.translate("my name is laobi");
+			//String result = WeixinUtil.translateFull("");
+//			System.out.println(result);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
